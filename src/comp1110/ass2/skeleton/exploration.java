ff93f69b59f2331ea50345cb36e39de432f55320
Nathan Woodburn <github@nathan.woodburn.au>
2023-03-15 13:22:30 +1100

    skeleton: Added exploration phase

---
+++ src/comp1110/ass2/skeleton/exploration.java
@@ -0,0 +1,56 @@
+package comp1110.ass2.skeleton;
+
+public class exploration {
+
+
+    /**
+     * This method is used to check if the placement of the tile is valid
+     * It checks if the tile is placed on water or land and if the tile is placed on a tile that is already occupied.
+     * It also checks if the tile is placed on a tile that is not adjacent to the tile that is already placed.     *
+     */
+    public boolean validPlacement() {
+        return false; // Needed to stop errors
+    }
+
+
+    /**
+     * This method picks the youngest player and gives them the first turn.
+     * It then asks them to place a tile.
+     * It then asks the next player to place a tile.
+     */
+    public static void main(String[] args) {
+
+    }
+
+     /**
+     * This method will be used to get the player to place a tile.
+     * It will use the validPlacement method to check if the placement is valid.
+     * It will also use the placeTile method to place the tile.
+     */
+    public void playerTurn(player player){
+
+    }
+
+
+    /**
+     * This method will be used to place a piece on a tile.
+     */
+    public void placePiece(tile tile, piece piece){
+
+    }
+
+    /**
+     * This method will be used to claim a stone circle.
+     */
+
+    public void claimStoneCircle(stoneCircle stoneCircle, player player){
+
+    }
+
+    /**
+     * This method will be used to check if exploration phase is over.
+     */
+    public boolean explorationPhaseOver() {
+        return false; // Needed to stop errors
+    }
+}
