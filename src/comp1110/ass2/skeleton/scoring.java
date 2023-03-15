2e32c07d95793ee1513934187411fd3c9d930094
Nathan Woodburn <github@nathan.woodburn.au>
2023-03-15 13:43:31 +1100

    skeleton: Added scoring class

---
+++ src/comp1110/ass2/skeleton/scoring.java
@@ -0,0 +1,42 @@
+package comp1110.ass2.skeleton;
+
+/**
+ * This class is used to score the game
+ *
+ */
+public class scoring {
+    /**
+     * This method is used to score the islands
+     */
+    public void scoreIslands(){
+
+    }
+
+    /**
+     * This method is used to score the piece links
+     */
+    public void scoreLinks(){
+
+    }
+
+    /**
+     * This method is used to score the owner of the island
+     */
+    public void scoreMajorities(){
+
+    }
+
+    /**
+     * This method is used to score the resources
+     */
+    public void scoreResources(){
+
+    }
+
+    /**
+     * This method is used to score the statuettes
+     */
+    public void scoreStatuettes(){
+
+    }
+}
