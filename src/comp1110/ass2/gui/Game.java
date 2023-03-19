7e60847379b0b062371bdde112bb9e9fb9771f7e
Nathan Woodburn <github@nathan.woodburn.au>
2023-03-19 21:46:36 +1100

    gui: Added title and set not resizable

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -17,6 +17,8 @@ public class Game extends Application {
     public void start(Stage stage) throws Exception {
         Scene scene = new Scene(this.root, WINDOW_WIDTH, WINDOW_HEIGHT);
         stage.setScene(scene);
+        stage.setTitle("Blue Lagoon");
+        stage.setResizable(false);
         stage.show();
     }
 }
