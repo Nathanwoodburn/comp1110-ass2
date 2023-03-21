626dc8ff4155a3baa64f104dcb49bf9018f1c805
Nathan Woodburn <github@nathan.woodburn.au>
2023-03-21 13:21:46 +1100

    gittest: Added main

--- src/gittest/B.java
+++ src/gittest/B.java
@@ -1,6 +1,11 @@
 package gittest;
 
 public class B {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
 
     @Override
     public String toString() {
