c9d78fc3a0b21307b3a3793379c1dfae705ad76b
Nathan Woodburn <github@nathan.woodburn.au>
2023-03-21 13:22:23 +1100

    gittest: Added comment

--- src/gittest/B.java
+++ src/gittest/B.java
@@ -1,7 +1,11 @@
 package gittest;
 
 public class B {
-    
+
+    /**
+     * Test method
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
